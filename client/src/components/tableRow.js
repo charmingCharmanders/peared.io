eda57dfbbcc3b89ff5d624f8019345d0bb4afa6c
Wesley Choi <weschoi.creative@gmail.com>
2017-07-17 15:46:20 -0700

    Add Front-End Skeleton (#7)
    * adding reactdom and react router, and added dashbord plus basic header
    
    * add front-end skeleton
    
    * add front-end skeleton

---
+++ client/src/components/tableRow.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {Table, ButtonToolbar, Button, Navbar, CollapsibleNav, NavItem, NavDropdown, Nav, MenuItem, Grid, Col, Row} from 'react-bootstrap';
+
+const TableRow = ({id, partner, name, time, category}) => (
+  <tr>
+    <td>{id}</td>
+    <td>{partner}</td>
+    <td>{name}</td>
+    <td>{time}</td>
+    <td>{category}</td>
+  </tr>
+);
+
+export default TableRow;
---
+++ client/src/components/view.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+
+
+class View extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>On the View Component</h2>
+        <ul>
+          <li><Link to='/roster'>Go to /Roster</Link></li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default View;
