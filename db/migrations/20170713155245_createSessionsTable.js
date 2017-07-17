4f3e7cef8fa33a78babb911d23834e95dde3dd9f
Jeff Shindelus <jeff.shindelus@gmail.com>
2017-07-17 12:19:32 -0700

    multiple commits to change environment variables and small changes for delpoying to heroku.
    add node version in package.json
    
    Setup Database with Dummy Data and Tests (#3)
    
    Implement prompt seed file and prompt test
    
    * Added tests and friends table, seed files, and specs
    
    * fix pomander error
    
    * merge conflict
    
    * remove old users seed file
    
    add prompts and sessions api (#4)
    
    * add tests for prompts api
    
    * add prompts api
    
    * add tests for sessions api
    
    * add sessions api
    
    * add default startedAt time to session model
    
    * add skeletonCode column to prompts migration
    
    * fix tests for users and prompts api
    
    * remove delete method from prompts api
    
    revert back to profiles table to fix local auth (#5)
    
    * revert back to profiles table to fix local auth
    
    * update profiles api
    
    * fix tests for auths
    
    fix merge conflicts
    
    modified package.json to not include pomander on post-install
    
    syntax error
    
    add env variable for postgres
    
    syntax error
    
    branch issues
    
    delete nodemon
    
    add yarn start to package.json
    
    merge conflict
    
    relocate package.json files
    
    minor changes with redis
    
    minor changes
    
    trying heroku logs
    
    try redirect for heroku
    
    capitalized port
    
    changed config file

--- db/migrations/20170713155245_createSessionsTable.js
+++ db/migrations/20170713155245_createSessionsTable.js
@@ -15,6 +15,7 @@ exports.up = function(knex, Promise) {
     })
   ]);
 };
+
 exports.down = function(knex, Promise) {
   return Promise.all([
     knex.schema.dropTable('sessions')
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,3 +1,7 @@
 module.exports.Profiles = require('./profiles');
 module.exports.Prompts = require('./prompts');
 module.exports.Sessions = require('./sessions');
+<<<<<<< HEAD
+=======
+module.exports.Users = require('./users');
+>>>>>>> multiple commits to change environment variables and small changes for delpoying to heroku.
