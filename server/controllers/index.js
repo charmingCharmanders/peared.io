3c6db937a60f9d1e05589e1ef5405b7ea8012f43
Jeff Shindelus <jeff.shindelus@gmail.com>
2017-07-18 12:47:32 -0700

    merging commits with rebase

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,7 @@
 module.exports.Profiles = require('./profiles');
 module.exports.Prompts = require('./prompts');
 <<<<<<< HEAD
+<<<<<<< HEAD
 module.exports.Sessions = require('./sessions');
 =======
 module.exports.Sessions = require('./sessions');
@@ -9,3 +10,6 @@ module.exports.Sessions = require('./sessions');
 module.exports.Users = require('./users');
 >>>>>>> multiple commits to change environment variables and small changes for delpoying to heroku.
 >>>>>>> multiple commits to change environment variables and small changes for delpoying to heroku.
+=======
+module.exports.Sessions = require('./sessions');
+>>>>>>> merging commits with rebase
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,3 @@ module.exports.auth = require('./auth');
 module.exports.profiles = require('./profiles');
 module.exports.prompts = require('./prompts');
 module.exports.sessions = require('./sessions');
-<<<<<<< HEAD
-=======
-module.exports.users = require('./users');
->>>>>>> multiple commits to change environment variables and small changes for delpoying to heroku.
