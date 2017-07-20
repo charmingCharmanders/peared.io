f96feb52202fb1134a27d3cf1772141c7edd8a86
Jeff Shindelus <jeff.shindelus@gmail.com>
2017-07-20 11:52:48 -0700

    implement basic video chat with tokbox iframe

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -4,18 +4,20 @@ import Header from './header';
 import HistoryTable from './historyTable';
 import FriendsList from './friendsList';
 import Stats from './stats';
+import Video from './video';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import {Table, ButtonToolbar, Button, Navbar, CollapsibleNav, NavItem, NavDropdown, Nav, MenuItem, Grid, Col, Row} from 'react-bootstrap';
 
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
+
   }
 
   render() {
     return (
       <div>
-        <Grid>      
+        <Grid>
           <Row className='show-grid'>
             <Col md={12} ><h6>Dashboard</h6></Col>
           </Row>
