191929fa8ff33b3ba6104333227cb7267e46fdeb
Wesley Choi <weschoi.creative@gmail.com>
2017-07-21 20:09:10 -0700

    add seed data for hints, solutions and tests

--- db/data/tests.js
+++ db/data/tests.js
@@ -9,5 +9,25 @@ module.exports = [
     description: 'it should add two negative numbers',
     arguments: '-2, -2',
     expectedOutput: -4
+  }, {
+    promptId: 2,
+    description: 'it should multiply one negative and one positive number',
+    arguments: '-5, 5',
+    expectedOutput: -20
+  }, {
+    promptId: 2,
+    description: 'it should multiply two positive numbers',
+    arguments: '2, 4',
+    expectedOutput: 8
+  }, {
+    promptId: 3,
+    description: 'it should return even for an even number',
+    arguments: '10',
+    expectedOutput: true
+  }, {
+    promptId: 3,
+    description: 'it should return odd for an odd number',
+    arguments: '3',
+    expectedOutput: false
   }
 ];
