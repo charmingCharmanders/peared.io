2c2569b481d58d1cbaa672474ef3c914dd7a4a39
Daniel Anderson <danieltanderson@berkeley.edu>
2017-07-22 15:37:41 -0700

    Test highlighting (#29)
    * working on tests
    
    * styled the tests a little bit, more to follow

--- client/src/components/consoleAndTest.js
+++ client/src/components/consoleAndTest.js
@@ -10,10 +10,19 @@ class ConsoleAndTest extends React.Component {
   render() {
     return (
       <div className="console">
+        <div className="testContainer">
         {this.props.testResults.map((test, index) => {
-          return <div key={index}>{test.description} <br />{test.result}</div>;
-        })
-      }
+
+          return (
+            <div key={index}>
+              <span className="testDescription">{index}: {test.description}</span>
+              <br />
+              <span className={(test.result ==='test passed' ? 'testPass' : 'testFail')}>   {test.result}</span>
+            </div>
+          );
+
+        })}
+        </div>
       </div>
     );
   }
