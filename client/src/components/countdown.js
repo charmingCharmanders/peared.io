c5c019ef77735253869e2dd2bfc3dd09f0001a04
David Vassett <david@davidvassett.com.au>
2017-07-31 10:11:28 -0700

    Remove unwanted function from countdown component

--- client/src/components/countdown.js
+++ client/src/components/countdown.js
@@ -38,15 +38,6 @@ class Countdown extends React.Component {
     }
   }
 
-  renderIframe() {
-    const hostname = window.location.hostname;
-    if (hostname === '127.0.0.1' || hostname === 'localhost') {
-      return <iframe src={`https://tokbox.com/embed/embed/ot-embed.js?embedId=f9ef23c6-1579-4b77-9f1f-8380c10fcbc0&room=${this.props.roomId}&iframe=true`} width='360' height='240'></iframe>;
-    } else {
-      return <iframe src={`https://tokbox.com/embed/embed/ot-embed.js?embedId=4808bf20-edc1-4fdd-8d6c-358c7c73c64a&room=${this.props.roomId}&iframe=true`} width='360' height='240'></iframe>;
-    }
-  }
-
   render() {
     return (
       this.state.currentCount ?
