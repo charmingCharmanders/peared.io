e31b3e3f448432fdd043899f67c87364a4dc3b3a
Wesley Choi <weschoi.creative@gmail.com>
2017-08-02 16:45:14 -0700

    remove logger from middleware (#66)

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,4 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import { logger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
 import React from 'react';
@@ -7,9 +6,17 @@ import App from './app';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import allReducers from './reducers';
+import { logger } from 'redux-logger';
+
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middleware = applyMiddleware(thunk, logger);
-const store = createStore(allReducers, composeEnhancers(middleware));
+let middleware;
 
+if (process.env.NODE_ENV !== 'production') {
+  middleware = applyMiddleware(thunk, logger);
+} else {
+  middleware = applyMiddleware(thunk);
+}
+
+const store = createStore(allReducers, composeEnhancers(middleware));
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
