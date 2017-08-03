18ec9b293a5b4d6792885691eb008049b12cfbf9
David Vassett <david@davidvassett.com.au>
2017-08-03 10:51:24 -0700

    update video component size to be relative

--- client/src/components/video.js
+++ client/src/components/video.js
@@ -8,16 +8,18 @@ class Video extends React.Component {
   renderIframe() {
     const hostname = window.location.hostname;
     if (hostname === '127.0.0.1' || hostname === 'localhost') {
-      return <iframe src={`https://tokbox.com/embed/embed/ot-embed.js?embedId=f9ef23c6-1579-4b77-9f1f-8380c10fcbc0&room=${this.props.roomId}&iframe=true`} width='300' height='200' frameBorder='0'></iframe>;
+      return <iframe src={`https://tokbox.com/embed/embed/ot-embed.js?embedId=f9ef23c6-1579-4b77-9f1f-8380c10fcbc0&room=${this.props.roomId}&iframe=true`} frameBorder='0'></iframe>;
     } else {
-      return <iframe src={`https://tokbox.com/embed/embed/ot-embed.js?embedId=4808bf20-edc1-4fdd-8d6c-358c7c73c64a&room=${this.props.roomId}&iframe=true`} width='300' height='200' frameBorder='0'></iframe>;
+      return <iframe src={`https://tokbox.com/embed/embed/ot-embed.js?embedId=4808bf20-edc1-4fdd-8d6c-358c7c73c64a&room=${this.props.roomId}&iframe=true`} frameBorder='0'></iframe>;
     }
   }
 
   render() {
     return (
       <div className="video-container">
-        {this.renderIframe()}
+        <div className="video-wrapper">
+          {this.renderIframe()}
+        </div>
       </div>
     );
   }
