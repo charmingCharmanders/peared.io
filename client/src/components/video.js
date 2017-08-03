9b3e2cdd013acc7cd03a43e86bcf314712289d3b
David Vassett <david@davidvassett.com.au>
2017-08-03 10:04:17 -0700

    clean video component

--- client/src/components/video.js
+++ client/src/components/video.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { LinkContainer } from 'react-router-bootstrap';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
-import {ButtonToolbar, Button, Navbar, CollapsibleNav, NavItem, NavDropdown, Nav, MenuItem, Grid, Col, Row} from 'react-bootstrap';
 
 class Video extends React.Component {
   constructor(props) {
